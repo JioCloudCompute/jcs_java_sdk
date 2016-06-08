3b398de7d32359e062708f5cf76f57b403c3e905
Suman <sumanswar12@gmail.com>
2016-06-08 12:54:55 +0530

    Corrected Wrapper Error

--- src/main/java/com/jcs_java_sdk/compute_api/model/ShowDeleteOnTerminationFlagResponse.java
+++ src/main/java/com/jcs_java_sdk/compute_api/model/ShowDeleteOnTerminationFlagResponse.java
@@ -1,45 +1,38 @@
 package com.jcs_java_sdk.compute_api.model;
 
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "ShowDeleteOnTerminationFlagResponse")
 public class ShowDeleteOnTerminationFlagResponse {
 	@XmlElement
 	private String requestId;
-
-	@XmlElement
-	private String instanceId;
-	@XmlElement
-	private String volumeId;
-	@XmlElement
-	private boolean deleteOnTermination;
+	
+	@XmlElement(name = "volume")
+	private VolumeTerminationInfo volume;
+	
 	public final String getRequestId() {
 		return requestId;
 	}
+	
 	public final String getInstanceId() {
-		return instanceId;
+		return volume.getInstanceId();
 	}
 	public final String getVolumeId() {
-		return volumeId;
+		return volume.getVolumeId();
 	}
 	public final boolean isDeleteOnTermination() {
-		return deleteOnTermination;
+		return volume.isDeleteOnTermination();
 	}
+	
 	void setRequestId(String requestId) {
 		this.requestId = requestId;
 	}
-	void setInstanceId(String instanceId) {
-		this.instanceId = instanceId;
+
+	void setVolume(VolumeTerminationInfo volume) {
+		this.volume = volume;
 	}
-	void setVolumeId(String volumeId) {
-		this.volumeId = volumeId;
-	}
-	void setDeleteOnTermination(boolean deleteOnTermination) {
-		this.deleteOnTermination = deleteOnTermination;
-	}
-	
+		
 	
 	
 }
--- src/main/java/com/jcs_java_sdk/compute_api/model/UpdateDeleteOnTerminationFlagResponse.java
+++ src/main/java/com/jcs_java_sdk/compute_api/model/UpdateDeleteOnTerminationFlagResponse.java
@@ -1,45 +1,36 @@
 package com.jcs_java_sdk.compute_api.model;
 
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "UpdateDeleteOnTerminationFlagResponse")
 public class UpdateDeleteOnTerminationFlagResponse {
 	
 	@XmlElement
 	private String requestId;
-	@XmlElementWrapper(name = "volume")
-	@XmlElement
-	private String instanceId;
-	@XmlElementWrapper(name = "volume")
-	@XmlElement
-	private String volumeId;
-	@XmlElementWrapper(name = "volume")
-	@XmlElement
-	private boolean deleteOnTermination;
+	
+	@XmlElement(name = "volume")
+	private VolumeTerminationInfo volume;
+	
 	public final String getRequestId() {
 		return requestId;
 	}
+	
 	public final String getInstanceId() {
-		return instanceId;
+		return volume.getInstanceId();
 	}
 	public final String getVolumeId() {
-		return volumeId;
+		return volume.getVolumeId();
 	}
 	public final boolean isDeleteOnTermination() {
-		return deleteOnTermination;
+		return volume.isDeleteOnTermination();
 	}
+	
 	void setRequestId(String requestId) {
 		this.requestId = requestId;
 	}
-	void setInstanceId(String instanceId) {
-		this.instanceId = instanceId;
-	}
-	void setVolumeId(String volumeId) {
-		this.volumeId = volumeId;
-	}
-	void setDeleteOnTermination(boolean deleteOnTermination) {
-		this.deleteOnTermination = deleteOnTermination;
+
+	void setVolume(VolumeTerminationInfo volume) {
+		this.volume = volume;
 	}
 
 
